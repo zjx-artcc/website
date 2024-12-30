fd65340646eef742639241f979977fdc26d26456
Aneesh Reddy <aneeshjreddy@gmail.com>
2024-12-30 16:25:39 -0500

    Updated scraper to support new system.

--- app/api/update/events/route.ts
+++ app/api/update/events/route.ts
@@ -1,12 +1,57 @@
 import {revalidatePath} from "next/cache";
 import {updateSyncTime} from "@/actions/lib/sync";
 import prisma from "@/lib/db";
+import { UTApi } from "uploadthing/server";
 
 export const dynamic = 'force-dynamic';
 
+const ut = new UTApi();
+
 export async function GET() {
 
-    // TODO
+    const in24Hours = new Date();
+    in24Hours.setHours(in24Hours.getHours() + 24);
+
+    const oneDayAgo = new Date();
+    oneDayAgo.setHours(oneDayAgo.getHours() - 24);
+
+    await prisma.event.updateMany({
+        where: {
+            manualPositionsOpen: false,
+            start: {
+                lte: in24Hours,
+            },
+            positionsLocked: false,
+        },
+        data: {
+            positionsLocked: true,
+        },
+    });
+
+    const archivedEvents = await prisma.event.findMany({
+        where: {
+            end: {
+                lte: oneDayAgo,
+            },
+        },
+    });
+
+    for (const event of archivedEvents) {
+        await ut.deleteFiles(event.bannerKey || '');
+
+        await prisma.event.update({
+            where: {
+                id: event.id,
+            },
+            data: {
+                archived: new Date(),
+                hidden: true,
+                positionsLocked: true,
+                manualPositionsOpen: false,
+                bannerKey: null,
+            },
+        });
+    };
 
     await updateSyncTime({events: new Date(),});
 
---
+++ prisma/migrations/20241230212403_update_positions_lock_flag/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Event" ALTER COLUMN "positionsLocked" SET DEFAULT true;

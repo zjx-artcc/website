b0c63463b566cfcf4896c17290cc08b663281b5d
Aneesh Reddy <aneeshjreddy@gmail.com>
2025-01-27 21:56:34 -0500

    Added continue statement

--- app/api/update/roster/route.ts
+++ app/api/update/roster/route.ts
@@ -43,6 +43,7 @@ export async function GET() {
                         },
                     },
                 });
+                continue;
             }
 
             await prisma.user.update({

b763fb2f1b8ee896e90cccd7dc2e3247b304e29e
Harry Xu <harryxu2626@gmail.com>
2025-02-19 23:57:53 -0500

    fixed missing text from SV pass emails

--- templates/TrainingSession/InstructorNotification.tsx
+++ templates/TrainingSession/InstructorNotification.tsx
@@ -7,11 +7,11 @@ export const instructorNotification = (student: User, session: TrainingSession, 
     return renderReactToMjml(
         <MultipleRecipientsEmailWrapper headerText="Training Session Passed">
             <p>Dear Instructors,</p>
-            <p><strong>{student.firstName} {student.lastName} ({student.cid})</strong>has passed
+            <p><strong>{student.firstName} {student.lastName} ({student.cid})</strong> has passed
                 lesson <strong>{lesson.name} ({lesson.identifier})</strong>.</p>
-            <br/>
-            <p>Quick Links</p>
+            <p>Quick Links:</p>
             <a href={`https://vzdc.org/training/sessions/${session.id}`}>Training Session</a>
+            <br/>
             <a href={`https://vzdc.org/training/controller/${student.cid}`}>Student's Certifications</a>
             <br/>
             <p>You are receiving this email because you are marked as an instructor and this lesson will notify you on

9fe0119e34c78254c51a2f340672da5edb2c0bd7
Vainnor <crafty_chick.0u@icloud.com>
2025-03-20 21:48:25 -0400

    updated credits

--- app/credits/page.tsx
+++ app/credits/page.tsx
@@ -1,9 +1,9 @@
 import {Card, CardContent, Grid2, Stack, Typography} from "@mui/material";
 import Link from "next/link";
 import {GitHub} from "@mui/icons-material";
-import {Aguafina_Script} from "next/font/google";
+import {Roboto} from "next/font/google";
 
-const fancyFont = Aguafina_Script({subsets: ['latin'], weight: ["400"]});
+const headingFont = Roboto({subsets: ['latin'], weight: ['400']});
 
 export default async function Home() {
 
@@ -24,20 +24,35 @@ export default async function Home() {
                     <CardContent>
                         <Typography variant="h4" gutterBottom>Main Contributors</Typography>
                         <Stack direction="column" spacing={8} sx={{m: 8,}}>
-                            <Typography variant="h5" fontWeight="bold" fontSize={50}
-                                        letterSpacing={9} {...fancyFont.style}>Aneesh Reddy <Link
+                            <Typography variant="h5" fontWeight="bold" 
+                                        letterSpacing={9} {...headingFont.style}>Aneesh Reddy <Link
                                 href="https://github.com/beabravedude" target="_blank"
                                 style={{color: 'inherit',}}><GitHub fontSize="large"/></Link></Typography>
-                            <Typography variant="h5" fontWeight="bold" fontSize={37}
-                                        letterSpacing={7} {...fancyFont.style}>Carson Berget <Link
+                            <Typography variant="h5" fontWeight="bold" 
+                                        letterSpacing={7} {...headingFont.style}>Carson Berget <Link
                                 href="https://github.com/Vainnor" target="_blank" style={{color: 'inherit',}}><GitHub
                                 fontSize="medium"/></Link></Typography>
-                            <Typography variant="h5" fontWeight="bold" letterSpacing={7} {...fancyFont.style}>Harry
+                            <Typography variant="h5" fontWeight="bold" letterSpacing={7} {...headingFont.style}>Harry
                                 Xu <Link href="https://github.com/harryxu2626" target="_blank"
                                          style={{color: 'inherit',}}><GitHub fontSize="small"/></Link></Typography>
-                            <Typography variant="h5" fontWeight="bold" letterSpacing={7} {...fancyFont.style}>Leo
+                            <Typography variant="h5" fontWeight="bold" letterSpacing={7} {...headingFont.style}>Leo
                                 Roberts <Link href="https://github.com/monty23monty" target="_blank"
                                               style={{color: 'inherit',}}><GitHub fontSize="small"/></Link></Typography>
+                        </Stack>
+                    </CardContent>
+                </Card>
+                <Card sx={{mt: 4}}>
+                    <CardContent>
+                        <Typography variant="h4" gutterBottom>ZJX Fork Contributors</Typography>
+                        <Stack direction="column" spacing={8} sx={{m: 8,}}>
+                            <Typography variant="h5" fontWeight="bold" 
+                                        letterSpacing={9} {...headingFont.style}>Samuel Valencia <Link
+                                href="https://github.com/svalencia014" target="_blank"
+                                style={{color: 'inherit',}}><GitHub fontSize="large"/></Link></Typography>
+                            <Typography variant="h5" fontWeight="bold" 
+                                        letterSpacing={7} {...headingFont.style}>Christopher Mangan<Link
+                                href="https://github.com/chrisjm66" target="_blank" style={{color: 'inherit',}}><GitHub
+                                fontSize="medium"/></Link></Typography>
                         </Stack>
                     </CardContent>
                 </Card>

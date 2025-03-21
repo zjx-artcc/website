9837a76a4aa692ff60eb9b71ab6560f6b1ed3196
Christopher Mangan <chrisjm66@gmail.com>
2025-03-21 00:53:57 -0400

    change site background

--- components/Hero/BackgroundImage.tsx
+++ components/Hero/BackgroundImage.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Image from "next/image";
-import bg from "@/public/img/home-bg.png";
+import bg from "@/public/img/home-bg.jpg";
 import {Box} from "@mui/material";
 
 export default function BackgroundImage() {
     return (
-        <Box sx={{zIndex: -10, overflow: 'hidden',}}>
-            <Image src={bg} alt="vZDC" fill style={{objectFit: 'contain', opacity: 0.3,}}/>
+        <Box sx={{zIndex: -10, overflow: 'hidden'}}>
+            <Image src={bg} alt="vZDC" fill style={{position: 'absolute', opacity: 0.3, filter: 'blur(1px)'}}/>
+            <Image src={bg} alt="vZDC" fill style={{position: 'absolute', top: '100%', opacity: 0.3, filter: 'blur(1px)'}}/>
         </Box>
     );
 }

bc247036a70957f1b2732ccb2d8a829252563831
Vainnor <crafty_chick.0u@icloud.com>
2025-03-21 10:04:28 -0400

    fixed tooltip in footer

--- components/Hero/BackgroundImage.tsx
+++ components/Hero/BackgroundImage.tsx
@@ -6,8 +6,8 @@ import {Box} from "@mui/material";
 export default function BackgroundImage() {
     return (
         <Box sx={{zIndex: -10, overflow: 'hidden'}}>
-            <Image src={bg} alt="vZDC" fill style={{position: 'absolute', opacity: 0.3, filter: 'blur(1px)'}}/>
-            <Image src={bg} alt="vZDC" fill style={{position: 'absolute', top: '100%', opacity: 0.3, filter: 'blur(1px)'}}/>
+            <Image src={bg} alt="vZJX" fill style={{position: 'absolute', opacity: 0.3, filter: 'blur(1px)'}}/>
+            <Image src={bg} alt="vZJX" fill style={{position: 'absolute', top: '100%', opacity: 0.3, filter: 'blur(1px)'}}/>
         </Box>
     );
 }

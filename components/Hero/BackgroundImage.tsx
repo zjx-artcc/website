ad755d53107e9612dbd42f23d638e2c7b4092479
Christopher Mangan <chrisjm66@gmail.com>
2025-03-24 18:19:09 -0400

    fix background

--- components/Hero/BackgroundImage.tsx
+++ components/Hero/BackgroundImage.tsx
@@ -5,9 +5,8 @@ import {Box} from "@mui/material";
 
 export default function BackgroundImage() {
     return (
-        <Box sx={{zIndex: -10, overflow: 'hidden'}}>
-            <Image src={bg} alt="vZDC" fill style={{position: 'absolute', opacity: 0.3, filter: 'blur(1px)'}}/>
-            <Image src={bg} alt="vZDC" fill style={{position: 'absolute', top: '100%', opacity: 0.3, filter: 'blur(1px)'}}/>
+        <Box sx={{zIndex: -10, overflow: 'hidden', position: 'fixed', top: 0, left: 0, right: 0, bottom: 0}}>
+            <Image src={bg} alt="vZDC" fill style={{opacity: 0.3, filter: 'blur(1px)'}}/>
         </Box>
     );
 }

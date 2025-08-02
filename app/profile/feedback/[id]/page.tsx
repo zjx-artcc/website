c5c53f7e03b16f5fac55a8b1a80389e61e607dcb
moicestdes <johnsone.destiny@gmail.com>
2025-08-02 17:43:14 -0400

    attempt 2 to fix type error

--- app/profile/feedback/[id]/page.tsx
+++ app/profile/feedback/[id]/page.tsx
@@ -24,6 +24,7 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
         },
         include: {
             controller: true,
+            pilot: true,
         },
     });
 

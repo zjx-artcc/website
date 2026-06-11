25936634569176095a410b5a672a59a02d59bfa5
Chris Mangan <chrisjm66@gmail.com>
2026-06-11 17:55:31 -0400

    Fix stripe erroring

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -2,4 +2,11 @@ import 'server-only';
 
 import Stripe from 'stripe';
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+export function getStripe() {
+  const secretKey = process.env.STRIPE_SECRET_KEY;
+  if (!secretKey) {
+    throw new Error('STRIPE_SECRET_KEY is not set');
+  }
+
+  return new Stripe(secretKey);
+}
